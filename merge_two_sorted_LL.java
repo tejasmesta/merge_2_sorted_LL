b711ca2ddee1d52ecdf8e96d2808fd9479262e09
Tejas Mesta <tejasmesta@gmail.com>
2022-07-21 19:48:22 +0530

    Add files via upload

---
+++ merge_two_sorted_LL.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null || l2==null)
+        {
+            return l1!=null?l1:l2;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        while(c1!=null && c2!=null)
+        {
+            if(c1.val<c2.val)
+            {
+                prev.next = c1;
+                c1 = c1.next;
+            }
+            else
+            {
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = c1!=null?c1:c2;
+        return dummy.next;
+    }
+}
